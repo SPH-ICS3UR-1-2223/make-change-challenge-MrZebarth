129fa2820b00c55eb50e695bee4f9ff41f4de258
MrZebarth <kris.zebarth@ocsb.ca>
2022-11-15 13:36:05 -0500

    Added penny rounding

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,21 @@ public class Main {
 		//Break the change into all denominations: 
 		//$100,$50,$20,$10,$5,$2,$1,$0.25,$0.10,$0.05
 		//As a bonus, make your project round to the nearest $0.05 like actual stores.
+//		System.out.println("Enter a payment");
+//		double payment = in.nextDouble();
+//		System.out.println("You paid $"+Math.round(payment*100)/100.0);
+		
+		//How do you get just one digit? HINT: %
+		double change = 123.44;
+		//shift the decimal place by 2, use the modulus. Make sure it's an int.
+		int lastDigit = (int)((change*100)%10);
+		//if output is 1 or 2 or 6 or 7, round down.
+		if (lastDigit==1 || lastDigit==2) {
+			change = change-(lastDigit/100.0);
+		}else if (lastDigit==3 || lastDigit==4) {
+			change = change + ((5-lastDigit)/100.0);
+		}
+		System.out.println(change);
 		
 	}
 
